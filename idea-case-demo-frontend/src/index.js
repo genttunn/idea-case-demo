7afc1f6823f2b505aeff70738d66a32ccdc54c44
Nghi Tran <nghi.tran@pplus.vn>
2019-03-25 23:37:12 +0200

    search feature done

--- idea-case-demo-frontend/src/index.js
+++ idea-case-demo-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
+
 import thunk from "redux-thunk";
 
 import "./index.css";
